ed09f648c3984021830f022a404685e6fb075f45
memyboi <pooface442@gmail.com>
2024-03-10 10:04:31 +0000

    didnt know you needed double quotes for that

--- installation.sh
+++ installation.sh
@@ -3,11 +3,12 @@
 GREEN='\033[0;32m'
 BLUE='\033[0;34m' 
 CLEAR='\033[0m'
-echo -e '${BLUE}> sudo chmod u+x ./fancygrim/fancygrim${CLEAR}'
+
+echo -e "${BLUE}> sudo chmod u+x ./fancygrim/fancygrim${CLEAR}"
 sudo chmod u+x ./fancygrim/fancygrim
-echo -e '${BLUE}> sudo mv ./fancygrim/fancygrim /usr/bin${CLEAR}'
+echo -e "${BLUE}> sudo mv ./fancygrim/fancygrim /usr/bin${CLEAR}"
 sudo mv ./fancygrim/fancygrim /usr/bin
-echo -e '${BLUE}> sudo rm -rf ./fancygrim${CLEAR}'
+echo -e "${BLUE}> sudo rm -rf ./fancygrim${CLEAR}"
 sudo rm -rf ./fancygrim
 echo -e "${GREEN}fancygrim is now installed!"
 echo -e "${GREEN}run the command 'fancygrim' to see usage!"
