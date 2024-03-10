52c7db82b099196f03c3e324c7aebb5e7ecabb4a
memyboi <pooface442@gmail.com>
2024-03-10 10:01:31 +0000

    fixed formatting w/ installation.sh

--- installation.sh
+++ installation.sh
@@ -1,9 +1,11 @@
 # yes i recycled this
 # i'm lazy ;-;
 GREEN='\033[0;32m'
+CLEAR='\033[0m'
 sudo chmod u+x ./fancygrim/fancygrim
 sudo mv ./fancygrim/fancygrim /usr/bin
 sudo rm -rf ./fancygrim
 echo -e "${GREEN}fancygrim is now installed!"
 echo -e "${GREEN}run the command 'fancygrim' to see basic system information!"
+echo -e "${CLEAR}\n"
 fancygrim
